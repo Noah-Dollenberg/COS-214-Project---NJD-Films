6eda139fdad71f9001a9cfa6a41553ff2aa31490
u04579624 <u04579624@tuks.co.za>
2025-10-31 11:26:08 +0200

    started working on doxygen

--- Strategy - Watering Methods/FrequentWatering.cpp
+++ Strategy - Watering Methods/FrequentWatering.cpp
@@ -1,6 +1,21 @@
+/**
+ * @file FrequentWatering.cpp
+ * @brief Implementation of the FrequentWatering class
+ * @author NJD Films
+ * @date 2024
+ */
+
 #include "FrequentWatering.h"
 #include <iostream>
 
+/**
+ * @brief Implements frequent watering care for plants
+ * 
+ * This method provides daily watering care by outputting a message
+ * indicating that the plant should be watered daily to keep the
+ * soil consistently moist. This strategy is ideal for plants that
+ * thrive in constantly moist conditions.
+ */
 void FrequentWatering::careForPlant() {
     std::cout << "Watering daily, keeping soil consistently moist\n";
 }
