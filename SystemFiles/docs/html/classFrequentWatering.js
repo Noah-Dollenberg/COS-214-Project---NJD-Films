14b16bbef1cb5008f995d8eac793d42002259915
DillonKoekemoer <167412949+DillonKoekemoer@users.noreply.github.com>
2025-11-02 11:35:12 +0200

    Removed the outdated doxygen from SystemFiles

--- SystemFiles/docs/html/classFrequentWatering.js
+++
@@ -1,5 +0,0 @@
-var classFrequentWatering =
-[
-    [ "~FrequentWatering", "classFrequentWatering.html#a160e253bb15baa0c4542d781852942fc", null ],
-    [ "careForPlant", "classFrequentWatering.html#aa4826cc93fb5fa5a3267f1acc4981c99", null ]
-];
